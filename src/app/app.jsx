23fbdaf18119171a6ba3cff77850e289c8d57425
Luceta <57941164+Luceta@users.noreply.github.com>
2022-01-10 00:01:54 +0900

    Style: Button module components

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Button from "../components/Button";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Button size="sm">기본</Button>
+      <Button size="md">테스트 2</Button>
+      <Button size="lg">테스트 2</Button>
+    </>
+  );
 }
 
 export default App;
