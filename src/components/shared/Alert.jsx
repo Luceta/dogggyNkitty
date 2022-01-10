49fc8fcea5c4a0c6d37d36eb981eb203321c7e1c
Luceta <57941164+Luceta@users.noreply.github.com>
2022-01-10 13:30:33 +0900

    💄Fix: alert modal container style changed

--- src/components/shared/Alert.jsx
+++ src/components/shared/Alert.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-function Alert({ open, close, header, children, title }) {
+function Alert({ title, actionContent }) {
   return (
     <ModalWrapper>
       <ModalTitle>{title}</ModalTitle>
@@ -10,7 +10,7 @@ function Alert({ open, close, header, children, title }) {
           취소
         </button>
         <button type="button" className="delete">
-          삭제
+          {actionContent}
         </button>
       </div>
     </ModalWrapper>
@@ -21,44 +21,39 @@ export default Alert;
 
 const ModalWrapper = styled.div`
   width: 252px;
-  height: 110px;
-  border-radius:10px;
-  background:yellowgreen;
+  border-radius: 10px;
+  border: 0.5px solid #dbdbdb;
+  overflow: hidden;
 
   .action-box {
-    border-top: 0.5px solid #DBDBDB;
-
+    display: flex;
+    border-top: 0.5px solid #dbdbdb;
+  }
   .cancel {
-    width: 126px;
+    flex: 1;
     height: 46px;
-    background: #FFFFFF;
+    background: #ffffff;
     outline: none;
-    border:none;
-    border: 0.5px solid #DBDBDB;
+    border: none;
+    border-right: 0.5px solid #dbdbdb;
     font-size: 14px;
     line-height: 18px;
-    border-bottom-left-radius:10px;
   }
   .delete {
-    width: 126px;
+    flex: 1;
     height: 46px;
-    background: #FFFFFF;
+    border: none;
+    background: #ffffff;
     color: #f26e22;
-    border:none;
-    border-bottom: 0.5px solid #DBDBDB;
-    border-top:0.5px solid #DBDBDB;
-    border-right:0.5px solid #DBDBDB;
-    border-left:none;
     font-size: 14px;
     line-height: 18px;
-    border-bottom-right-radius:10px;
   }
 `;
 
 const ModalTitle = styled.h3`
   margin: 0;
-  margin-bottom: 22px;
-  padding: 22px 0 0 61px;
+  padding: 22px 0;
+  text-align: center;
   font-size: 16px;
   font-weight: 400;
   line-height: 20px;
