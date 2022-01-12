d9253af2968bb7d9b181ff9e3ebb87d3a5570375
Luceta <57941164+Luceta@users.noreply.github.com>
2022-01-12 17:14:23 +0900

    ✨Add: Splah component

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 import Statusbar from "../Statusbar/Statusbar";
 
 function Splash(props) {
   return (
     <>
       <Statusbar />
-      <h1>
-        <img src="/assets/full-logo2x.png" alt="logo" />
-      </h1>
+      <SplashContainer>
+        <h1>
+          <img src="/assets/full-logo2x.png" alt="logo" />
+        </h1>
+      </SplashContainer>
     </>
   );
 }
+
+const SplashContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Splash;
